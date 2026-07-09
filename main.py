600bac9210a9c220a31ba0a34675d8aafbb85db6
pbeltran82 <pbeltran82@users.noreply.github.com>
2026-07-08 19:20:54 -0700

    Register decision engine routes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from api.app import app
 from api.risk_gate import register_risk_gate
+from api.decision_engine import register_decision_engine
 
 register_risk_gate(app)
+register_decision_engine(app)
